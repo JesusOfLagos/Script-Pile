22ff76a3184e1fe38982d3082042bbdbe1654a9b
Akanle Jesus <105521093+JesusOfLagos@users.noreply.github.com>
2023-02-01 18:40:43 +0100

    Update Boolean.js

--- Boolean.js
+++ Boolean.js
@@ -2,4 +2,7 @@ let age = 10;
 
 if (age == 10) console.log("Age is equal to ten");
 else console.log("Age is not equal to ten");
+age = 20;
 
+if (age == 10) console.log("Age is equal to ten");
+else console.log("Age is not equal to ten");
