112580d47477a9cebb8c4909c987e2277d944d59
Akanle Jesus <105521093+JesusOfLagos@users.noreply.github.com>
2023-02-01 18:21:11 +0100

    Update object.js

--- object.js
+++ object.js
@@ -11,4 +11,7 @@ example.charAt(0);
 ["Beans" "Soup" "Salad" "Meat" "Rice" "Spaghetti"];
 example.charAt(1);
 example.charAt(100);
-example.
+example.substring(1, 4);
+example.substring(1, 2);
+example.substring();
+
