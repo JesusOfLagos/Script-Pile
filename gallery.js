11ac61aa05fc0ff8614c356875785d879cc125f8
Akanle Jesus <105521093+JesusOfLagos@users.noreply.github.com>
2023-02-24 22:00:27 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,1 +1,36 @@
 
+const wahala = "Limit";
+
+console.log(wahala);
+
+
+
+function greet(){
+    console.log("There is no problem.");
+}
+
+greet();
+
+function addOne(){
+   let num1 = 5;
+    console.log(num1 + 1);
+}
+
+addOne();
+
+
+let attendance = prompt("Are you attending the conference?");
+
+const attendanceCount = e.attendance;
+
+if (attendance){
+for(let i = 20; i > 100; i++){
+    console.log(attendanceCount.attendance);
+}
+    
+}
+{
+firstNum = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+console.log(indexOf(6))
+}
